e02f30b4271e18e2e805295140ae1053ae69c053
Dima Lunkin <timonbandit@users.noreply.github.com>
2020-06-10 21:53:55 +0300

    Update main.js

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -254,8 +254,8 @@ $(document).ready(function() {
       AR: "https://belliata.com.ar/sign-up?utm_source=bss&utm_medium=link&utm_campaign=login-lp",
       CA: "https://belliata.ca/sign-up?utm_source=bss&utm_medium=link&utm_campaign=login-lp",
       ZA: "https://belliata.co.za/sign-up?utm_source=bss&utm_medium=link&utm_campaign=login-lp",
-      PL: "https://belliata.pl/sign-up?utm_source=bss&utm_medium=link&utm_campaign=detect",
-      AU: "https://belliata.com.au/sign-up?utm_source=bss&utm_medium=link&utm_campaign=detect",
+      PL: "https://belliata.pl/sign-up?utm_source=bss&utm_medium=link&utm_campaign=login-lp",
+      AU: "https://belliata.com.au/sign-up?utm_source=bss&utm_medium=link&utm_campaign=login-lp",
       CO: "https://belliata.co/sign-up?utm_source=bss&utm_medium=link&utm_campaign=login-lp",
       default: "https://belliata.com/sign-up?utm_source=bss&utm_medium=link&utm_campaign=login-lp"
     };
